a774c5f683e770b3bb01fbe067dcf78b9fc5f039
Nihar Gavva <nihargavva@gmail.com>
2026-08-12 14:10:12 -0400

    Refactor filter mask for improved readability

--- Python/app.py
+++ Python/app.py
@@ -19,11 +19,7 @@ year_range = st.sidebar.slider("Year", min_year, max_year, (min_year, max_year))
 fuel_types = sorted(df["engine_fuel_type"].unique())
 selected_fuel_types = st.sidebar.multiselect("Fuel Type", fuel_types, default=fuel_types)
 
-mask = (
-    df["make"].isin(selected_makes)
-    & df["year"].between(year_range[0], year_range[1])
-    & df["engine_fuel_type"].isin(selected_fuel_types)
-)
+mask = df["make"].isin(selected_makes) & df["year"].between(year_range[0], year_range[1]) & df["engine_fuel_type"].isin(selected_fuel_types)
 filtered_df = df[mask]
 
 count = len(filtered_df)
